c256dac6112754e8676fbd2673beeb8f6e0af932
Justin Webb <webbju@gmail.com>
2014-01-14 20:22:15 +0000

    Migrated project layout so that VS2010 specific files are named appropriately.

--- src/AndroidPlusPlus.VsDebugLauncherX/premake5.lua
+++
@@ -1,9 +0,0 @@
-external "AndroidPlusPlus.VsDebugLauncherX"
-
-  location "../AndroidPlusPlus.VsDebugLauncherX"
-  
-  uuid "7C701BB4-BAD8-40DA-AFDC-B9820A04F005"
-  
-  kind "SharedLib"
-  
-  language "C#"
