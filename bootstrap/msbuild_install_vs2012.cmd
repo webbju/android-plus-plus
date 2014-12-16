7ce2a47da75449d70c1f1fc8c2bfe6e2edc4758f
Justin Webb <webbju@gmail.com>
2014-12-16 15:01:21 +0000

    - (MSBuild) Ensured VS2012 & VS2013 bootstrap tools install to 'default' VS2010 directories too.

--- bootstrap/msbuild_install_vs2012.cmd
+++ bootstrap/msbuild_install_vs2012.cmd
@@ -2,4 +2,4 @@
 :: Bootstrap MSBuild support for Visual Studio 2012
 :: 
 
-"%ANDROID_PLUS_PLUS%\msbuild\bin\AndroidPlusPlus.MsBuild.Exporter.exe" --template-dir "%ANDROID_PLUS_PLUS%/msbuild/scripts/" --version-file "%ANDROID_PLUS_PLUS%\version.txt" --vs-version 2012
+"%ANDROID_PLUS_PLUS%\msbuild\bin\AndroidPlusPlus.MsBuild.Exporter.exe" --template-dir "%ANDROID_PLUS_PLUS%/msbuild/scripts/" --version-file "%ANDROID_PLUS_PLUS%\version.txt" --vs-version 2010 --vs-version 2012
