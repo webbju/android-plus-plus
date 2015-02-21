cda6f0fcfe4dda3bae3e2874986eb9415f344876
Justin Webb <webbju@gmail.com>
2015-02-21 14:20:19 +0000

    Added VS2015 projects

---
+++ bootstrap/msbuild_install_vs2015.cmd
@@ -0,0 +1,5 @@
+:: 
+:: Bootstrap MSBuild support for Visual Studio 2013
+:: 
+
+"%ANDROID_PLUS_PLUS%\msbuild\bin\AndroidPlusPlus.MsBuild.Exporter.exe" --template-dir "%ANDROID_PLUS_PLUS%/msbuild/scripts/" --version-file "%ANDROID_PLUS_PLUS%\version.txt" --vs-version 2010 --vs-version 2015
---
+++ bootstrap/msbuild_uninstall_vs2015.cmd
@@ -0,0 +1,5 @@
+:: 
+:: Bootstrap MSBuild support for Visual Studio 2013
+:: 
+
+"%ANDROID_PLUS_PLUS%\msbuild\bin\AndroidPlusPlus.MsBuild.Exporter.exe" --uninstall --vs-version 2015
