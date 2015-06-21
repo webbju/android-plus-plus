1ecee154c7d6578fa2ed3a2d549a7cf27e1679bc
Justin Webb <webbju@gmail.com>
2015-06-21 16:31:58 +0100

    Added NDK r10e GDB 7.7 binaries.

---
+++ contrib/gdb/bin/x86_64/7.7.0/mips64el-linux-android-gdb.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+call ..\bootstrap_python.cmd
+
+%~dp0\mips64el-linux-android-gdb-orig.exe %*
---
+++ contrib/gdb/bin/x86_64/7.7.0/x86_64-linux-android-gdb.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+call ..\bootstrap_python.cmd
+
+%~dp0\x86_64-linux-android-gdb-orig.exe %*
