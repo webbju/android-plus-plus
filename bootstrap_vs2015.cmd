868692f028f3577c7e254e16f7274aa006074df6
Justin Webb <webbju@gmail.com>
2015-07-29 18:16:43 +0100

    Added 'bootstrap_vs2015.cmd'

---
+++ bootstrap_vs2015.cmd
@@ -0,0 +1,21 @@
+:: 
+:: Bootstrap Android++ support for Visual Studio 2013
+:: 
+
+@echo off
+
+cd %~dp0
+
+set ANDROID_PLUS_PLUS=%CD%
+
+setx ANDROID_PLUS_PLUS "%CD%"
+
+setlocal
+
+call .\bootstrap\msbuild_install_vs2015.cmd
+
+call .\bootstrap\extension_uninstall_vs2015.cmd
+
+call .\bootstrap\extension_install_vs2015.cmd
+
+endlocal
