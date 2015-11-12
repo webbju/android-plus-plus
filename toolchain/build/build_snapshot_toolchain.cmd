5ac9af42f722f6529d8ef8a583d62b5fc910670c
Justin Webb <webbju@gmail.com>
2015-11-12 13:08:25 +0000

    - Modified toolchain projects to build with VisualStudio 2013 by default.

--- toolchain/build/build_snapshot_toolchain.cmd
+++ toolchain/build/build_snapshot_toolchain.cmd
@@ -2,12 +2,8 @@
 :: Build a snapshot of Android++ for MSBuild
 :: 
 
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ../src/app-jar-dependencies/app-jar-dependencies.sln /build "Release|x86"
+"%VS120COMNTOOLS%..\IDE\devenv.exe" ../src/app-java-builder/app-java-builder.sln /build "Release|x86"
 
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ../src/app-javac-dependencies/app-javac-dependencies.sln /build "Release|x86"
+"%VS120COMNTOOLS%..\IDE\devenv.exe" ../src/app-ndk-depends/app-ndk-depends.sln /build "Release|Win32"
 
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ../src/app-java-builder/app-java-builder.sln /build "Release|x86"
-
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ../src/app-ndk-depends/app-ndk-depends.sln /build "Release|Win32"
-
-"%VS100COMNTOOLS%..\IDE\devenv.exe" ../src/app-zipalign/app-zipalign.sln /build "Release|Win32"
+"%VS120COMNTOOLS%..\IDE\devenv.exe" ../src/app-zipalign/app-zipalign.sln /build "Release|Win32"
