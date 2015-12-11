342d9d5e5dce96985445d931fde8e560a95bd760
Justin Webb <webbju@gmail.com>
2015-12-11 12:21:43 +0000

    Re-added batch files for uninstalling the extension from Visual Studio 2010 installations.

---
+++ bootstrap/extension_uninstall_vs2010.cmd
@@ -0,0 +1,5 @@
+:: 
+:: Uninstall the Visual Studio 2010 debugger extension
+:: 
+
+%~dp0\utilities\extension_uninstall.cmd 2010 AndroidPlusPlus.VsIntegratedPackage..e57f7102-96b3-4d11-b153-0957bea37363 %*
