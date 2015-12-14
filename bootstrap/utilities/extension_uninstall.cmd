8667ca2876045abd8980fb10b3f9316b05aedf71
Justin Webb <webbju@gmail.com>
2015-12-14 16:49:52 +0000

    - Fixed extension batch files not having registered installation env-vars for VS2010.

--- bootstrap/utilities/extension_uninstall.cmd
+++ bootstrap/utilities/extension_uninstall.cmd
@@ -5,23 +5,29 @@
 @set VS_VERSION=%1
 @set EXTENSION_PACKAGE=%2
 
-@if %VS_VERSION% == 2013 (
-  @echo Uninstalling VS2013 extension
+@if %VS_VERSION% == 2010 (
+  @set DEVENV_PATH="%VS100COMNTOOLS%..\IDE\devenv.exe"
+  @set VSIX_PATH="%VS100COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @goto uninstall
+) else if %VS_VERSION% == 2012 (
+  @set DEVENV_PATH="%VS110COMNTOOLS%..\IDE\devenv.exe"
+  @set VSIX_PATH="%VS110COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @goto uninstall
+) else if %VS_VERSION% == 2013 (
   @set DEVENV_PATH="%VS120COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS120COMNTOOLS%..\IDE\VSIXInstaller.exe"
   @goto uninstall
 ) else if %VS_VERSION% == 2015 (
-  @echo Uninstalling VS2015 extension
   @set DEVENV_PATH="%VS140COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS140COMNTOOLS%..\IDE\VSIXInstaller.exe"
   @goto uninstall
-) 
 ) else (
   @echo ** Unknown or unspecified Visual Studio version (%VS_VERSION%)
 )
 @goto exit
 
 :uninstall
+@echo Uninstalling VS%VS_VERSION% extension
 %VSIX_PATH% /uninstall:%EXTENSION_PACKAGE% /quiet 
 @if %ERRORLEVEL% == 0 (
   @echo ** %VSIX_PATH% returned %ERRORLEVEL% [success]
