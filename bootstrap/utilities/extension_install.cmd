2a144afef48e39439170ba7adb531389f08fadf3
Justin Webb <webbju@gmail.com>
2016-01-08 23:13:35 +0000

    - Resolved a problem where bootstrapping extensions needed to be more explicit.

--- bootstrap/utilities/extension_install.cmd
+++ bootstrap/utilities/extension_install.cmd
@@ -9,18 +9,22 @@
 @if %VS_VERSION% == 2010 (
   @set DEVENV_PATH="%VS100COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS100COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=
   @goto install
 ) else if %VS_VERSION% == 2012 (
   @set DEVENV_PATH="%VS110COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS110COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=
   @goto install
 ) else if %VS_VERSION% == 2013 (
   @set DEVENV_PATH="%VS120COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS120COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=/skuVersion:12.0 /skuName:Ultimate /skuName:Premium /skuName:Pro
   @goto install
 ) else if %VS_VERSION% == 2015 (
   @set DEVENV_PATH="%VS140COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS140COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=/skuVersion:14.0 /skuName:Enterprise /skuName:Ultimate /skuName:Premium /skuName:Pro /skuName:Community
   @goto install
 ) else (
   @echo ** Unknown or unspecified Visual Studio version (%VS_VERSION%)
@@ -29,7 +33,7 @@
 
 :install
 @echo Installing VS%VS_VERSION% extension
-%VSIX_PATH% /admin %QUIET% %EXTENSION_PATH%
+%VSIX_PATH% /admin %QUIET% %VSIX_VERSION_ARGS% %EXTENSION_PATH%
 @if %ERRORLEVEL% == 0 (
   @echo ** %VSIX_PATH% returned %ERRORLEVEL% [success]
   @goto refreshextensions
--- bootstrap/utilities/extension_uninstall.cmd
+++ bootstrap/utilities/extension_uninstall.cmd
@@ -4,22 +4,27 @@
 
 @set VS_VERSION=%1
 @set EXTENSION_PACKAGE=%2
+@set QUIET=/quiet 
 
 @if %VS_VERSION% == 2010 (
   @set DEVENV_PATH="%VS100COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS100COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=
   @goto uninstall
 ) else if %VS_VERSION% == 2012 (
   @set DEVENV_PATH="%VS110COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS110COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=
   @goto uninstall
 ) else if %VS_VERSION% == 2013 (
   @set DEVENV_PATH="%VS120COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS120COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=/skuVersion:12.0 /skuName:Ultimate /skuName:Premium /skuName:Pro
   @goto uninstall
 ) else if %VS_VERSION% == 2015 (
   @set DEVENV_PATH="%VS140COMNTOOLS%..\IDE\devenv.exe"
   @set VSIX_PATH="%VS140COMNTOOLS%..\IDE\VSIXInstaller.exe"
+  @set VSIX_VERSION_ARGS=/skuVersion:14.0 /skuName:Enterprise /skuName:Ultimate /skuName:Premium /skuName:Pro /skuName:Community
   @goto uninstall
 ) else (
   @echo ** Unknown or unspecified Visual Studio version (%VS_VERSION%)
@@ -28,7 +33,7 @@
 
 :uninstall
 @echo Uninstalling VS%VS_VERSION% extension
-%VSIX_PATH% /uninstall:%EXTENSION_PACKAGE% /quiet 
+%VSIX_PATH% %VSIX_VERSION_ARGS% /uninstall:%EXTENSION_PACKAGE% %QUIET%
 @if %ERRORLEVEL% == 0 (
   @echo ** %VSIX_PATH% returned %ERRORLEVEL% [success]
   @goto refreshextensions
